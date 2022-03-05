895de0d39c69cf04b98f0236a340e52497fdfd00
Mariraj-Krishnan <98956597+Mariraj-Krishnan@users.noreply.github.com>
2022-03-05 21:57:46 +0530

    Dhhd

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ audio.addEventListener("play", () => {
     timeOutId.push(
       window.setTimeout(() => {
         container.textContent = lines[i];
-      }, timing[i] - 100 - delay)
+      }, timing[i] - delay)
     );
   }
   played = true;
